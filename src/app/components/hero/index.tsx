7e312c81d3b2d05e11a35b8c0817177342309a31
cuginoAle <alessio.carnevale@mnscorp.net>
2024-03-05 09:22:34 +0100

    wip

--- src/app/components/hero/index.tsx
+++ src/app/components/hero/index.tsx
@@ -1,24 +1,19 @@
 import Image from 'next/image';
 import styles from './style.module.css';
 import logo from '@/assets/logo.webp';
+import hero from '@/assets/hero.webp';
 const Hero = () => {
   return (
     <div className="relative flex justify-center items-center">
-      <Image
-        src="https://source.unsplash.com/random/1580x600/?greece&20islands?sig=19"
-        width={1280}
-        height={600}
-        alt=""
-      />
+      <Image src={hero} width={1280} height={531} alt="" />
 
       <div className={styles.copyWrapper}>
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-3 lg:gap-4 items-center">
           <Image
             src={logo}
+            className={styles.logo}
             alt="Meltemi travel"
-            width={80}
-            height={80}
-            className="shadow-ale rounded-full"
+            sizes="(max-width: 400px) 40px, (max-width: 768px) 80px, 80px"
           />
           <h1 className={styles.title}>Meltemi travel</h1>
         </div>
